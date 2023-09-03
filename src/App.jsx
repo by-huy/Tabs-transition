7c3c8fbf8cee94247e7d27f31b62559d42a3f322
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-09-03 11:36:41 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div className="px-[2vw] text-[#fefce0]">
+      <div className="text-[1vw] leading-tight overflow-hidden">
+        <h1 className=" text-display font-getai">VIVIDGLIMPS</h1>
+      </div>
+      <p className="text-subheading">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus,
+        distinctio aliquam voluptate, porro a soluta sapiente esse.
+      </p>
+      <h2 className="mt-[2em] text-heading-1 font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+      <p className="text-body lg:w-[80ch]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem sit quasi deserunt vitae nisi fuga reprehenderit, ducimus cumque facere, animi perferendis quidem ab quae, quis iusto? Obcaecati magnam sunt fugit.</p>
+    </div>
+  );
+}
